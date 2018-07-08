0270153225b3f7f032a8bc8ef6375ac21ec4cb80
zql <774556731@qq.com>
2018-07-08 21:50:33 +0800

    侧边栏aside展示

--- src/views/home.vue
+++ src/views/home.vue
@@ -10,7 +10,93 @@
         </el-row>
     </el-header>
     <el-container>
-        <el-aside class="aside" width="200px">Aside</el-aside>
+        <!-- <el-aside class="aside" width="200px">Aside</el-aside> -->
+        <el-aside class="aside" width="200px">
+            <el-menu
+                default-active="/users"
+                class="menu"
+                unique-opened="ture"
+                router="ture">
+                <el-submenu index="1">
+                    <template slot="title">
+                    <i class="el-icon-menu"></i>
+                    <span>用户管理</span>
+                    </template>
+                    <el-menu-item index="/users">
+                        <template slot="title">
+                        <i class="el-icon-menu"></i>
+                            用户列表
+                        </template>
+                    </el-menu-item>
+                </el-submenu>
+                <el-submenu index="2">
+                    <template slot="title">
+                    <i class="el-icon-menu"></i>
+                    <span>权限管理</span>
+                    </template>
+                    <el-menu-item index="/roles">
+                        <template slot="title">
+                        <i class="el-icon-menu"></i>
+                            角色列表
+                        </template>
+                    </el-menu-item>
+                    <el-menu-item index="/rights">
+                        <template slot="title">
+                        <i class="el-icon-menu"></i>
+                            权限列表
+                        </template>
+                    </el-menu-item>
+                </el-submenu>
+                <el-submenu index="3">
+                    <template slot="title">
+                    <i class="el-icon-menu"></i>
+                    <span>商品管理</span>
+                    </template>
+                    <el-menu-item index="3-1">
+                        <template slot="title">
+                        <i class="el-icon-menu"></i>
+                            商品列表
+                        </template>
+                    </el-menu-item>
+                    <el-menu-item index="3-2">
+                        <template slot="title">
+                        <i class="el-icon-menu"></i>
+                            分类参数
+                        </template>
+                    </el-menu-item>
+                    <el-menu-item index="3-3">
+                        <template slot="title">
+                        <i class="el-icon-menu"></i>
+                            商品分类
+                        </template>
+                    </el-menu-item>
+                </el-submenu>
+                <el-submenu index="4">
+                    <template slot="title">
+                    <i class="el-icon-menu"></i>
+                    <span>订单管理</span>
+                    </template>
+                    <el-menu-item index="4-1">
+                        <template slot="title">
+                        <i class="el-icon-menu"></i>
+                            订单列表
+                        </template>
+                    </el-menu-item>
+                </el-submenu>
+                <el-submenu index="5">
+                    <template slot="title">
+                    <i class="el-icon-menu"></i>
+                    <span>数据统计</span>
+                    </template>
+                    <el-menu-item index="5-1">
+                        <template slot="title">
+                        <i class="el-icon-menu"></i>
+                            数据报表
+                        </template>
+                    </el-menu-item>
+                </el-submenu>
+            </el-menu>
+        </el-aside>
         <el-main class="main">Main</el-main>
     </el-container>
     </el-container>
@@ -49,6 +135,9 @@ export default {
     }
     .aside {
         background-color: #d3dce6;
+    }
+    .aside .menu {
+        height: 100%;
     }
     .main {
         background-color: #e9eef3;
